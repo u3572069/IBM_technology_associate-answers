d298c4c7ec8f2497705e4a4766fb526020101a0c
u3572069 <55791618+u3572069@users.noreply.github.com>
2022-11-11 19:37:50 +0800

    added solution

---
+++ solution2/solution.py
@@ -0,0 +1,25 @@
+def isValid(record):
+    k = record.split()
+    if(len(k)==3 and k[1] == "false"):
+        return False
+    else:
+        return True
+
+x = int(input())
+l = []
+for x in range(0,x):
+    l.append(input())
+allValid = True
+errorCodes = []
+for record in l:
+    if(allValid == True):
+        if (isValid(record) == False):
+            allValid = False
+    if isValid(record) == False:
+        errorCodes.append(record.split()[-1])
+
+if allValid == True:
+    print("Yes")
+else:
+    print("No")
+    print(*errorCodes)
